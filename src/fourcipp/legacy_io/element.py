52df7bceceb6e45ed82e113be2d8f3084879038d
Gil Robalo Rei <gil.rei@tum.de>
2025-03-25 18:01:46 +0100

    add element io

---
+++ src/fourcipp/legacy_io/element.py
@@ -0,0 +1,91 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2025 FourCIPP Authors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""Element io."""
+
+from fourcipp import CONFIG
+from fourcipp.legacy_io.inline_dat import (
+    casting_factory,
+    inline_dat_read,
+    to_dat_string,
+)
+
+_ELEMENT_CASTING = None
+if CONFIG["4C_metadata"] is not None:
+    _ELEMENT_CASTING = casting_factory(CONFIG["4C_metadata"]["legacy_element_specs"])
+
+from loguru import logger
+
+
+def read_element(line, elements_casting=_ELEMENT_CASTING):
+    """Read a element line.
+
+    Args:
+        line (str): Inline dat description of the element
+        elements_casting (dict): Element casting dict.
+
+    Returns:
+        dict: element as dict
+    """
+    line_list = line.split()
+
+    # First entry is always the element id starting from 1
+    element_id = int(line_list.pop(0))
+
+    # Second entry is always the element type
+    element_type = line_list.pop(0)
+
+    # Third entry is the cell type
+    cell_type = line_list.pop(0)
+
+    element_parameter_casting = elements_casting[element_type][cell_type]
+
+    element = {
+        "id": element_id,
+        "type": element_type,
+        "cell_type": cell_type,
+        "connectivity": element_parameter_casting[cell_type](line_list),
+    } | inline_dat_read(line_list, element_parameter_casting)
+
+    logger.info("hello")
+    return element
+
+
+def write_element(element):
+    """Write element as inline dat style.
+
+    Args:
+        element (dict): Element description
+
+    Returns:
+        str: element line
+    """
+    line = " ".join(
+        [
+            to_dat_string(element.pop("id")),
+            to_dat_string(element.pop("type")),
+            to_dat_string(element.pop("cell_type")),
+            to_dat_string(element.pop("connectivity")),
+        ]
+    )
+    for k, v in element.items():
+        line += " " + k + " " + to_dat_string(v)
+    return line
