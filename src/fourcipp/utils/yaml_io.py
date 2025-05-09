579104197174877706a9dd7c2bf13a622808c56e
Gil Robalo Rei <gil.rei@tum.de>
2025-05-09 10:06:58 +0200

    feat: change to 4C yaml style

--- src/fourcipp/utils/yaml_io.py
+++ src/fourcipp/utils/yaml_io.py
@@ -21,10 +21,12 @@
 # THE SOFTWARE.
 """YAML io."""
 
+import io
 import json
 import pathlib
 
 import ruamel.yaml
+import ruamel.yaml.representer
 import ryml
 
 
@@ -50,22 +52,45 @@ def load_yaml(path_to_yaml_file):
     return data
 
 
-def dump_yaml(data, path_to_yaml_file, sort_keys=False):
-    """Dump yaml to file.
+def _use_square_braces(data):
+    """Check if sequence should use '-' or '[ ]'."""
+    if isinstance(data, list):
+        return_val = True
+        for entry in data:
+            return_val = return_val and _use_square_braces(entry)
+            if not return_val:
+                break
+        return return_val
 
-    Args:
-        data (dict): Data to dump.
-        path_to_yaml_file (str): Yaml file path
-        sort_keys (bool): If true sort the sections by section name
-    """
+    elif (
+        isinstance(data, (int, float)) and not isinstance(data, bool)
+    ) or data is None:
+        return True
+    else:
+        return False
+
+
+class FourCYamlRepresenter(ruamel.yaml.RoundTripRepresenter):
+
+    def represent_sequence(self, tag, data, flow_style=None):
+        if _use_square_braces(data):
+            return super().represent_sequence(tag, data, flow_style=True)
+        else:
+            return super().represent_sequence(tag, data, flow_style=False)
+
+
+def get_4C_yaml(data, sort_keys=False):
     # Ignore alias or anchors in dumps
     ruamel.yaml.representer.RoundTripRepresenter.ignore_aliases = lambda x, y: True
+
+    # Set the desired sequence dump type
+    ruamel.yaml.representer.RoundTripRepresenter = FourCYamlRepresenter
 
     # Currently using ruamel.yaml, will probably change in the future
     YAML = ruamel.yaml.YAML()
 
     # Some nice settings
-    YAML.indent(mapping=2, sequence=4, offset=2)
+    YAML.indent(offset=2, sequence=4, mapping=2)
 
     # Avoid wrapping of long lines
     YAML.width = 4096
@@ -74,6 +99,31 @@ def dump_yaml(data, path_to_yaml_file, sort_keys=False):
     if sort_keys:
         data = {key: data[key] for key in sorted(data.keys())}
 
-    # Dump it
-    with open(path_to_yaml_file, "w", encoding="utf-8") as f:
-        YAML.dump(data, f)
+    buffer = io.StringIO()
+    YAML.dump(data, buffer)
+    return buffer.getvalue()
+
+
+def dump_yaml(data, path_to_yaml_file, sort_keys=False):
+    """Dump yaml to file.
+
+    Args:
+        data (dict): Data to dump.
+        path_to_yaml_file (str): Yaml file path
+        sort_keys (bool): If true sort the sections by section name
+    """
+    pathlib.Path(path_to_yaml_file).write_text(
+        get_4C_yaml(data, sort_keys=sort_keys), encoding="utf-8"
+    )
+
+
+data_examples = {
+    "a": [1, 2, 0.3, None],
+    "b": [1, 2, "xxxx", "aa"],
+    "c": ["a", "b"],
+    "cd": "b",
+    "cdc": {"b": 35, "l": 451},
+    "d": [[[1, 2], [3, None]], [True, False]],
+}
+
+print(get_4C_yaml(data_examples))
